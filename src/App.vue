0e3456eb8e1f7a93e3a965c1d93ef2d0ce6918ea
Adam Hegedus <adamhegedus@vivaldi.net>
2020-04-10 22:22:56 +0200

    Modify homepage content

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,82 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <nav id="nav">
+      <ul class="navbar">
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/about">About</router-link>
+        </li>
+      </ul>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import url("./grid.css");
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap");
+
+body {
+  font-family: Raleway, Helvetica, Arial, sans-serif;
+  background-color: whitesmoke;
+  color: #241c15;
+}
+
+address {
+  font-style: normal;
+}
+
+a {
+  color: dodgerblue;
+  text-decoration: none;
+}
+
+a:hover,
+a:active {
+  text-decoration: underline;
+  color: lighten($color: dodgerblue, $amount: 12);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  // color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  // padding: 30px;
+  background-color: dodgerblue;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    // color: #2c3e50;
+    color: white;
 
     &.router-link-exact-active {
-      color: #42b983;
+      background-color: lighten($color: dodgerblue, $amount: 12);
+      // color: #42b983;
+    }
+  }
+}
+
+.navbar {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  li {
+    display: inline;
+
+    a {
+      display: inline-block;
+      padding: 15px;
+
+      &:hover {
+        text-decoration: none;
+        background-color: lighten($color: dodgerblue, $amount: 12);
+      }
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <section>
+      <h2>Nyitvatartás</h2>
+      <ul class="business-hours">
+        <li>Hétfő - Péntek</li>
+        <li>
+          <time>9:00</time> -
+          <time>17:00</time>
+        </li>
+        <li>Ebédszünet</li>
+        <li>
+          <time>12:30</time> -
+          <time>13:30</time>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h2>Elérhetőség</h2>
+      <h3>Telefon</h3>
+      <a href="tel:+36204418166" title="Munkaidőben hívható telefonszám">+36 20 441-8166</a>
+      <h3>Cím</h3>
+      <address>
+        Régi Cifrakapu utca 64/A
+        <br />3300 Eger
+      </address>
+      <h3>GPS koordináta</h3>
+      <address><a href="https://www.google.hu/maps/place/47°54'52.7%22N+20°22'35.6%22E/@47.914632,20.3759948,147m/data=!3m2!1e3!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d47.9146324!4d20.3765419" title="A szerviz GPS koordinátái">47°54'52.7"N 20°22'35.6"E</a></address>
+      Térkép
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
